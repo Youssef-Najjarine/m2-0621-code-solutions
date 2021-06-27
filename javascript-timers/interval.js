52f15ce6d97f64b62a4f82aa3885da7360f1915b
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-06-27 08:31:26 -0700

    Completed javascript-timers.

---
+++ javascript-timers/interval.js
@@ -0,0 +1,17 @@
+const h1 = document.querySelector('h1');
+
+setInterval(handleSetInterval, 1000);
+function handleSetInterval() {
+if (h1.textContent === '4') {
+  h1.textContent = '3';
+} else if (h1.textContent === '3') {
+  console.log('hi');
+  h1.textContent = '2';
+} else if (h1.textContent === '2') {
+  h1.textContent = '1';
+} else if (h1.textContent === '1') {
+  h1.textContent = '~Earth Beeeelooowww Us~';
+} else if (h1.textContent === '~Earth Beeeelooowww Us~') {
+  h1.textContent = '4';
+}
+}
