05be27ba0241ea0fb690edc299238bb26a170cb7
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-06-28 16:35:38 -0700

    Made changes you requested.

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,7 +1,8 @@
 const h1 = document.querySelector('h1');
 
-setInterval(handleSetInterval, 1000);
+let myTimer = setInterval(handleSetInterval, 1000);
 function handleSetInterval() {
+  console.log('hi');
 if (h1.textContent === '4') {
   h1.textContent = '3';
 } else if (h1.textContent === '3') {
@@ -11,7 +12,6 @@ if (h1.textContent === '4') {
   h1.textContent = '1';
 } else if (h1.textContent === '1') {
   h1.textContent = '~Earth Beeeelooowww Us~';
-} else if (h1.textContent === '~Earth Beeeelooowww Us~') {
-  h1.textContent = '4';
+clearInterval(myTimer);
 }
 }
