1722e7a3638d735f498883cfd1072b3c2e971429
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-06-28 18:21:06 -0700

    Removed all console.logs

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -2,11 +2,9 @@ const h1 = document.querySelector('h1');
 
 let myTimer = setInterval(handleSetInterval, 1000);
 function handleSetInterval() {
-  console.log('hi');
 if (h1.textContent === '4') {
   h1.textContent = '3';
 } else if (h1.textContent === '3') {
-  console.log('hi');
   h1.textContent = '2';
 } else if (h1.textContent === '2') {
   h1.textContent = '1';
