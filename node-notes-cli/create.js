46eddf9dd4a47b96ea376da43931e4b767407f76
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-08-04 10:46:21 -0700

    if user tries to submit an undefined note or an empty string it returns console.log('Error, please enter a valid entry.')

--- node-notes-cli/create.js
+++ node-notes-cli/create.js
@@ -2,8 +2,12 @@ const data = require('./data.json');
 const fs = require('fs');
 
 function createNotes() {
-  data.notes[data.nextId] = process.argv[3];
-  data.nextId++;
+  if (process.argv[3] === undefined || process.argv[3] === '' || process.argv[3] === ' ') {
+    return console.log('Error, please enter a valid entry.');
+  } else {
+    data.notes[data.nextId] = process.argv[3];
+    data.nextId++;
+  }
   fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
     if (err) throw err;
   }
