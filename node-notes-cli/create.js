163c502edafa95f4eee58752d224b6ce46df3694
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-08-04 11:36:55 -0700

    Modified create entry function so all words after 'create' gets added into a string that gets saved to data.json.

--- node-notes-cli/create.js
+++ node-notes-cli/create.js
@@ -5,7 +5,13 @@ function createNotes() {
   if (process.argv[3] === undefined || process.argv[3] === '' || process.argv[3] === ' ') {
     return console.log('Error, please enter a valid entry.');
   } else {
-    data.notes[data.nextId] = process.argv[3];
+    let str = '';
+     str += process.argv[3] + ' ';
+    for (let i = 4; i < process.argv.length; i++) {
+     str += process.argv[i] + ' ';
+    }
+    let result = str.trim();
+    data.notes[data.nextId] = result;
     data.nextId++;
   }
   fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
