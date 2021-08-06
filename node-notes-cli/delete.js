d00f4a4c52edb98c1a52f189159d77feba2c3749
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-08-05 19:25:21 -0700

    Updated create and update files so they are more flexible with submitting entires. If a number isnt specified or is undefined or is NaN the update function will return an error message. If you try and create a new note but leave it undefined or as an empty string it will return an error message as well.

--- node-notes-cli/delete.js
+++ node-notes-cli/delete.js
@@ -1,12 +1,25 @@
 const data = require('./data.json');
 const fs = require('fs');
-
 function deleteNotes() {
-  delete data.notes[process.argv[3]];
- data.nextId--;
+ if (process.argv[3] === undefined) {
+   return console.log('An index number is required after typing delete.Please ensure that you have entered a number to delete.');
+ }
+
+ for (let i = 3; i < process.argv.length; i++) {
+
+   if (Number.isNaN(Number(process.argv[i]))) {
+        return console.log(process.argv[i],' is not a valid index. Please ensure all process.argv indexes are valid numbers.');
+    } else {
+     for (let property in data.notes) {
+       if (process.argv[i] === property) {
+         delete data.notes[process.argv[i]];
+       }
+     }
+    }
+  }
+  data.nextId--;
   fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
     if (err) throw err;
-    console.log('Note has been deleted');
   }
   );
 }
--- node-notes-cli/update.js
+++ node-notes-cli/update.js
@@ -2,11 +2,24 @@ const data = require('./data.json');
 const fs = require('fs');
 
 function updateNotes() {
-  data.notes[process.argv[3]] = process.argv[4];
-  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
-    if (err) throw err;
-    console.log('Note has been updated');
+  if (process.argv[3] === undefined || Number.isNaN(Number(process.argv[3])) || process.argv[3] === '' || process.argv[3] === ' ') {
+    return console.log('Error, please enter a valid index to update.');
+  } else {
+    let str = '';
+    str += process.argv[4] + ' ';
+    for (let i = 5; i < process.argv.length; i++) {
+      str += process.argv[i] + ' ';
+    }
+    let result = str.trim();
+    data.notes[process.argv[3]] = result;
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+      if (err) throw err;
+      console.log('Note has been updated');
+    }
+    );
   }
-  );
+
+
 }
 module.exports = updateNotes;
