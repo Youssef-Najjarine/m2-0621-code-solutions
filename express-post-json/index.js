df9094f15246e497e5ce8b46c452bbc3b757af1f
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-08-12 20:30:23 -0700

    Completed express-post-json.

---
+++ express-post-json/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+var nextId = 1;
+var grades = {};
+
+app.use(express.json());
+app.get('/api/grades', function (req, res) {
+
+  let gradesArray = [];
+  for (const property in grades) {
+    gradesArray.push(grades[property]);
+  }
+  res.json(gradesArray);
+})
+
+
+app.post('/api/grades', function (req, res) {
+  req.body.id = nextId;
+  nextId++;
+  res.statusCode = 201;
+  res.send(req.body);
+
+})
+
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!');
+});
+// localhost:3000/api/grades
+// name="Minecraft Steve" course="Smash Bros." score=64
